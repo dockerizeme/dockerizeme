55253459d844576502f68fe64448847673499375
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 21:55:42 +0000

    Adding tarfile member sanitization to extractall()

--- hard-gists/1e7c18c6f6eda87f1cb4995b0e6a22a5/snippet.py
+++ hard-gists/1e7c18c6f6eda87f1cb4995b0e6a22a5/snippet.py
@@ -109,7 +109,26 @@ print('Downloading dataset')
 if not os.path.exists(archive_folder):
     with urllib.request.urlopen(url, timeout=15) as stream, \
             tarfile.open(fileobj=stream, mode='r|gz') as tf:
-        tf.extractall(base_folder)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, base_folder)
 
 print('Initializing model')
 model = Model(opt.ntoken, opt.nhid, nclasses)
--- hard-gists/3fe84c05013fa87d74a8/snippet.py
+++ hard-gists/3fe84c05013fa87d74a8/snippet.py
@@ -33,7 +33,26 @@ def get_zeal_docsets_dir():
 def download_docset(url, directory):
     with urllib.request.urlopen(url) as response:
         with tarfile.open(fileobj=response, mode='r:gz') as tar:
-            tar.extractall(directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, directory)
 
 
 def search_url(choice, user_docsets):
